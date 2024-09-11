d09445345e1ba47091532b296b05ba2e61006d56
Boli27 <judiemu27@gmail.com>
2024-09-10 21:03:50 -0500

    Cambio del funcionamiento de los mensajes(Como objetos)

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 const WebSocket = require("ws");
-// Creamos el servidor WebSocked
+// Creamos el servidor WebSocket
 const PORT = 8080;
 const server = new WebSocket.Server({ port: PORT });
 
 let clients = [];
 
-//evento cuando un cliente se conect
 server.on("connection", (socket) => {
-  console.log("Nuevo Cliente conectado");
+  let userName = '';
+  // Agregar el cliente a la lista de clientes
   clients.push(socket);
 
-  //manejar los mensajes recibidos de los clientes
+  // Al recibir un mensaje
   socket.on("message", (message) => {
-    console.log(`Mensaje recibido: ${message}`);
-    //enviar el mensaje a todos los demas clientes conectados
-    clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    const data = JSON.parse(message);
+
+    if (data.type === 'name') {
+      userName = data.name; // Guardamos el nombre del usuario
+      console.log(`Nuevo usuario conectado: ${userName}`);
+
+      // Notificar a todos los demás clientes que un nuevo usuario ha entrado al chat
+      clients.forEach((client) => {
+        if (client !== socket && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: 'notification', message: `${userName} ha entrado al chat.` }));
+        }
+      });
+    } else if (data.type === 'message') {
+      console.log(`Mensaje recibido de ${userName}: ${data.message}`);
+      // Enviar el mensaje a todos los demás clientes conectados
+      clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: 'message', name: userName, message: data.message }));
+        }
+      });
+    }
   });
 
-  // Manejar la desconeccion de los clientes
+  // Manejar la desconexión de los clientes
   socket.on("close", () => {
-    console.log("Cliente Desconectado");
+    console.log(`${userName} se ha desconectado`);
     clients = clients.filter((client) => client !== socket);
+
+    // Notificar a los demás usuarios de la desconexión
+    clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: 'notification', message: `${userName} ha salido del chat.` }));
+      }
+    });
   });
 });
 
